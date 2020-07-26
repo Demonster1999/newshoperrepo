e7141fdc7b86757b243771f4ac30ac13d55baa0a
rishi  namdev <namdev373@gmail.com>
2020-07-26 10:37:00 +0530

    new update

--- js/scrollnav.js
+++ js/scrollnav.js
@@ -1,49 +1,15 @@
-tooltip_options={
-    delay: { "show": 500, "hide": 100 },
-    trigger: 'manual',
-    animate:false,
-    container:'body'
-}
+$(document).ready(function () {
 
-$('li.active').tooltip(tooltip_options).tooltip('enable')
+    $(window).scroll(function () {
 
+        var height = $('.intro-to-app').height();
+        var scrollTop = $(window).scrollTop();
 
+        if (scrollTop >= height - 400) {
+            $('.navbar').addClass('solid-nav');
+        } else {
+            $('.navbar').removeClass('solid-nav');
+        }
 
-
-function changeDot() {
-    const scrollValue = $(window).scrollTop();
-    const heightSec2 = $('#about').offset().top;
-    const heightSec3 = $('#services').offset().top;
-    const heightSec4 = $('#contact').offset().top;
-    $('li').tooltip('hide');
-    if (scrollValue < heightSec2) {
-        $('nav li').not('.dot1').removeClass('active');
-        $('.dot1').addClass('active');
-        $('li.active').tooltip('show');
-    } else if (scrollValue < heightSec3) {
-        $('nav li').not('.dot2').removeClass('active');
-        $('.dot2').addClass('active');
-        $('li.active').tooltip('show');
-    } else if (scrollValue < heightSec4) {
-        $('nav li').not('.dot3').removeClass('active');
-        $('.dot3').addClass('active');
-        $('li.active').tooltip('show');
-    } else  {
-        $('nav li').not('.dot4').removeClass('active');
-        $('.dot4').addClass('active');
-        $('li.active').tooltip('show');
-    }
-    
-}
-
-$(window).on("scroll", changeDot)
-
-$('nav li').on('click', function () {
-    const goToSection = '.s' + $(this).attr('id');
-    $('body, html').animate({
-        scrollTop: $(goToSection).offset().top + 1
-       
-    })
-   
-})
-
+    });
+});
